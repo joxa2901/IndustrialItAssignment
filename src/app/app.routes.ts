ca18f5475f49a846672b160e7e1a4459792c8b15
joxa2901 <joxa2901@gmail.com>
2025-12-05 12:46:24 +0100

    added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login/login';
+import { Home } from './home/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: Login,
+  },
+  {
+    path: 'home',
+    component: Home,
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+];

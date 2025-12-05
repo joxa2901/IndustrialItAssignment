b2d523dade4e4f8de96efad740d22da57a68d8a4
joxa2901 <joxa2901@gmail.com>
2025-12-05 13:16:11 +0100

    home page in progress

--- src/app/home/home/home.ts
+++ src/app/home/home/home.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { HomeService } from '../../services/home-service';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrl: './home.scss',
 })
 export class Home {
+  homeService = inject(HomeService);
+  currentPage = 1;
+  limit = 10;
 
+  constructor() {
+    this.loadPage();
+  }
+
+  loadPage() {
+    const skip = (this.currentPage - 1) * this.limit;
+    this.homeService.getAnnouncements(this.limit, skip);
+  }
+
+  next() {
+    this.currentPage++;
+    this.loadPage();
+  }
+
+  prev() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.loadPage();
+    }
+  }
 }
